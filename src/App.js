f70ce0b63bd754fc47f22feb7c7f36c0ca9ca1b2
nhellbach <nielshellbach@googlemail.com>
2024-04-01 02:14:20 +0200

    zweiter commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. erster Test
+          Edit <code>src/App.js</code> and save to reload. zweiter Test
         </p>
         <a
           className="App-link"
